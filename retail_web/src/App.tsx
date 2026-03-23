d9406e17770e3fd428f3c1a517b3f2555c862d5c
m2_project <949613532@qq.com>
2026-03-23 14:29:07 +0800

    Feat(web): Implement authentication context and routes

--- retail_web/src/App.tsx
+++ retail_web/src/App.tsx
@@ -1,22 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import { ProductList } from "./pages/ProductList";
 import { ProductDetail } from "./pages/ProductDetail";
 import { Cart } from "./pages/Cart";
 import { Checkout } from "./pages/Checkout";
 import { OrderResult } from "./pages/OrderResult";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 import { AdminPlaceholder } from "./pages/AdminPlaceholder";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/orders/:id" element={<OrderResult />} />
-        <Route path="/admin/*" element={<AdminPlaceholder />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orders/:id" element={<OrderResult />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin/*" element={<AdminPlaceholder />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
